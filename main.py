c1543d9f9952086e1fac4abebe4c9699c631a2fe
Muga117 <exiagardofficial@gmail.com>
2025-11-04 08:25:38 +0300

    Modify main.py
    Modified main.py to add a tune argument option
    in the argument parser.

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import argparse
+
+import optuna
 from train import train_agent
 from test import  test_agent
+from tune import objective
 
 def main():
     parser = argparse.ArgumentParser(description="Run Sonic RL Project")
-    parser.add_argument("--mode", choices=["train", "test"], required=True,
-                        help="Choose whether to train or test the agent")
+    parser.add_argument("--mode", choices=["train", "test", "tune"], required=True,
+                        help="Choose whether to train, test, or hypertune the agent"
+    )
     args = parser.parse_args()
 
     if args.mode == "train":
         train_agent()
     elif args.mode == "test":
         test_agent()
+    elif args.mode == "tune":
+        study = optuna.create_study(
+            direction="maximize",
+            sampler=optuna.samplers.TPESampler(multivariate=True, n_startup_trials=5),
+            pruner=optuna.pruners.MedianPruner(n_startup_trials=5)
+        )
+        study.optimize(objective, n_trials=10)  
+        print("Best hyperparameters:", study.best_params)
+
 
 if __name__ == "__main__":
     main()
