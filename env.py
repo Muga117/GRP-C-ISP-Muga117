c0e4ede32af7e366d6874ee85c6589c584e24450
Muga117 <exiagardofficial@gmail.com>
2025-11-16 22:18:37 +0300

    Modify env.py
    Modified env.py by adding list with Sonic games and their zones. Added wrap function to env.py.

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 
+import random
 from paths import VIDEOS_PATH
 from wrappers import AllowBacktracking, RewardScaler, SonicDiscretizer, StochasticFrameSkip, WarpFrame, RandomLevelWrapper
 from gymnasium.wrappers import TimeLimit
@@ -7,49 +8,56 @@ import numpy as np
 
 ZONES = [
     "AngelIslandZone.Act1",
-    "AngelIslandZone.Act2",
+    "MarbleGardenZone.Act1",
     "HydrocityZone.Act1",
-    #"MarbleGardenZone.Act1",
-    #"CarnivalNightZone.Act1",
-    #"IcecapZone.Act1",
-    #"LaunchBaseZone.Act1",
-    #"MushroomHillZone.Act1",
-    #"FlyingBatteryZone.Act1",
-    #"SandopolisZone.Act1",
-    #"LavaReefZone.Act1",
-    #"DeathEggZone.Act1"
+    "MushroomHillZone.Act1",
 ]
 
-def make_env(record_video=False, video_folder=VIDEOS_PATH, max_episode_steps=4500, render_mode=None):
-    #state = np.random.choice(ZONES)
+GAME_TO_ZONES = {
+    "SonicTheHedgehog-Genesis": [
+        "GreenHillZone.Act1",
+        "GreenHillZone.Act2",
+        "GreenHillZone.Act3",
+    ],
+    "SonicTheHedgehog2-Genesis": [
+        "EmeraldHillZone.Act1",
+        "EmeraldHillZone.Act2",
+    ],
+    "SonicAndKnuckles3-Genesis": [
+        "AngelIslandZone.Act1",
+    ],
+}
+
+def make_env(game, state, render_mode="human"):
     env = retro.make(
-        game='SonicAndKnuckles3-Genesis',
-        #state=state,
-        state="AngelIslandZone.Act2",
+        game=game,
+        state=state,
         scenario='contest',
         use_restricted_actions=retro.Actions.ALL,
         players=1,
         record=False,
         render_mode=render_mode
-        #render_mode='human'  
     )
-    print("Creating Environment")
     return env
-    #env = RandomLevelWrapper(ZONES)
 
-def wrap_env(env, max_episode_steps=4500, scale_rew=True):
+def wrap_env(env, max_episode_steps=4500):
     env = SonicDiscretizer(env)
+    env = RewardScaler(env)
     env = StochasticFrameSkip(env, n=4, stickprob=0.25)
     if max_episode_steps is not None:
         env = TimeLimit(env, max_episode_steps=max_episode_steps)
     env = WarpFrame(env, width=96, height=96, grayscale=True)
     env = AllowBacktracking(env)
-    #if scale_rew:
-        #env = RewardScaler(env)
     return env
 
-def make_env_fn(render_mode=None, record_video=False):
-    env = make_env(record_video=record_video, render_mode=render_mode)
+def make_train_env(zone, render_mode):
+    env = make_env(state=zone, render_mode=render_mode)
     return wrap_env(env)
- 
 
+def make_test_env():
+    #game = "SonicTheHedgehog-Genesis"
+    #game = "SonicTheHedgehog2-Genesis"
+    game = "SonicAndKnuckles3-Genesis"
+    state = GAME_TO_ZONES[game][0]
+    env = make_env(game=game, state=state, render_mode="human")
+    return wrap_env(env)
