be68e896a3f4c01e8b44ad9578684bbb5024593f
Muga117 <exiagardofficial@gmail.com>
2025-11-16 22:12:35 +0300

    Remove tune.py and Modify main.py
    Removed tune.py and updated main.py to remove references to tuning functionality.

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import argparse
 import optuna
 from train import train_agent
 from test import  test_agent
-from tune import objective
+#from tune import objective
 
 def main():
     parser = argparse.ArgumentParser(description="Run Sonic RL Project")
-    parser.add_argument("--mode", choices=["train", "test", "tune"], required=True,
-                        help="Choose whether to train, test, or hypertune the agent"
+    parser.add_argument("--mode", choices=["train", "test"], required=True,
+                        help="Choose whether to train or test the agent"
     )
     args = parser.parse_args()
 
@@ -16,15 +16,6 @@ def main():
         train_agent()
     elif args.mode == "test":
         test_agent()
-    elif args.mode == "tune":
-        study = optuna.create_study(
-            direction="maximize",
-            sampler=optuna.samplers.TPESampler(multivariate=True, n_startup_trials=5),
-            pruner=optuna.pruners.MedianPruner(n_startup_trials=5)
-        )
-        study.optimize(objective, n_trials=10)  
-        print("Best hyperparameters:", study.best_params)
-
 
 if __name__ == "__main__":
     main()
